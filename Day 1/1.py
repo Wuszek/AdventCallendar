f07076e728754b9c0b2fe2fd2d1bbf385ed5cec1
wiktor.kobiela <wuch.mail@gmail.com>
2020-12-05 18:24:36 +0100

    Day1 part2 excercise

--- Day 1/1.py
+++ Day 1/1.py
@@ -7,11 +7,12 @@ def prize(L):
 
     for x in L:
         for y in L:
-            # if x != y | To prevent 1 + 1 and 2 + 2 (following example)
-            # (y, x) not in pair_list | Prevent inverted pairs
-            if x != y and x + y == C and (y, x) not in pair_list:
-                pair_list.append((x, y))
-                multiplied = x*y
+            for z in L:
+                # if x != y | To prevent 1 + 1 and 2 + 2 (following example)
+                # (y, x) not in pair_list | Prevent inverted pairs
+                if x != y and x != z and z != y and x + y + z== C and (y, x, z) not in pair_list:
+                    pair_list.append((x, y, z))
+                    multiplied = x*y*z
 
     return pair_list, multiplied
 
