96f6f845d3016f942fe7e2bebe6f29cc4d5e224e
wiktor.kobiela <wuch.mail@gmail.com>
2020-12-07 17:53:35 +0100

    Day5 part1 excercise

---
+++ Day 5/5.py
@@ -0,0 +1,55 @@
+with open('input.txt') as f:
+    list_input = [line for line in f.read().splitlines()]
+
+
+def seats_checker(L):
+    n = 0
+    m = 0
+    seat_id_list = []
+
+    for x in L:
+
+        search_seat = list_input[n]
+        #print(search_seat)
+
+        seats_pools = []
+        column_pools = []
+        seats_pools.extend(range(0, 128))
+        column_pools.extend(range(0, 8))
+
+        for y in search_seat:
+            #print(m)
+            if search_seat[m] == "F":
+                seats_pools = seats_pools[:len(seats_pools) // 2]
+                #print(seats_pools)
+                m += 1
+
+            elif search_seat[m] == "B":
+                seats_pools = seats_pools[len(seats_pools) // 2:]
+                #print(seats_pools)
+                m += 1
+
+            elif search_seat[m] == "L":
+                column_pools = column_pools[:len(column_pools) // 2]
+                #print(column_pools)
+                m += 1
+
+            elif search_seat[m] == "R":
+                column_pools = column_pools[len(column_pools) // 2:]
+                #print(column_pools)
+                m += 1
+
+            if m == 10:
+                #print(seats_pools, column_pools)
+                seat_id_list.append((int(seats_pools[0])*8)+int(column_pools[0]))
+                n += 1
+                m = 0
+
+    print(seat_id_list)
+    return seat_id_list
+
+def highest_id(ID_list):
+    print(max(ID_list))
+
+
+highest_id(seats_checker(list_input))
