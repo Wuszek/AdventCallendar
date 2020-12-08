7da5a20350ef7cbb2aa6c61a2728940dbd3959e3
wiktor.kobiela <wuch.mail@gmail.com>
2020-12-08 20:43:19 +0100

    Day6 part1 excercise

---
+++ Day 6/6.py
@@ -0,0 +1,29 @@
+with open('input.txt') as f:
+    list_input = [line for line in f.read().splitlines()]
+
+#print(list_input)
+
+def passport_checker(L):
+
+    n = 0
+    sum_string = ""
+    counts_sum = 0
+    for x in L:
+        temp_string = list_input[n]
+        #print(temp_string)
+
+        if temp_string != "":
+            sum_string = sum_string + temp_string
+            #print(sum_string)
+            n += 1
+
+        else:
+           unique = len(set(sum_string))
+           #print(unique)
+           sum_string = ""
+           n += 1
+           counts_sum += unique
+
+    print(counts_sum)
+
+passport_checker(list_input)
