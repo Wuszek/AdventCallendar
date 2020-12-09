a10583157f7927fb5fad898e2aedb33827e72b29
wiktor.kobiela <wuch.mail@gmail.com>
2020-12-09 12:59:27 +0100

    Day6 part2 excercise

---
+++ Day 6/6_2.py
@@ -0,0 +1,40 @@
+from collections import Counter
+
+with open('input.txt') as f:
+    list_input = [line for line in f.read().splitlines()]
+
+def answer_counter(L):
+
+    freq = {}
+    sum_string = ""
+    counts_sum = 0
+    people_count = 0
+    for x in L:
+
+        if x != "":
+            sum_string = sum_string + x
+            people_count += 1
+
+        elif x =="":
+            if people_count == 1:
+                #print(sum_string)
+                unique = len(set(sum_string))
+                sum_string = ""
+                counts_sum += unique
+                people_count = 0
+
+            elif people_count > 1:
+
+                for keys in sum_string:
+                    freq[keys] = freq.get(keys, 0) + 1
+
+                for x in freq:
+                     if freq.get(x, 0) == people_count:
+                         counts_sum += 1
+                freq = {}
+                sum_string = ""
+                people_count = 0
+
+    print("Suma:" + str(counts_sum))
+
+answer_counter(list_input)
