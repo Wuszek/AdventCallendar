4558a6ee37185b1e6e45ee210198edd59daae930
wiktor.kobiela <wuch.mail@gmail.com>
2020-12-09 09:55:59 +0100

    Day6 part1 removed spaghetti code

--- Day 6/6.py
+++ Day 6/6.py
@@ -1,27 +1,17 @@
 with open('input.txt') as f:
     list_input = [line for line in f.read().splitlines()]
 
-#print(list_input)
-
 def answer_counter(L):
 
-    n = 0
     sum_string = ""
     counts_sum = 0
     for x in L:
-        temp_string = list_input[n]
-        #print(temp_string)
 
-        if temp_string != "":
-            sum_string = sum_string + temp_string
-            #print(sum_string)
-            n += 1
-
+        if x != "":
+            sum_string = sum_string + x
         else:
            unique = len(set(sum_string))
-           #print(unique)
            sum_string = ""
-           n += 1
            counts_sum += unique
 
     print(counts_sum)
